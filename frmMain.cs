afdb864440555b399bf59da74b1f1972397d008d
sg <srfngrc@outlook.com>
2019-05-16 19:19:04 -0400

    2nd step

---
+++ frmMain.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap03_BookExamplesPROJ
+{
+    public class frmMain : Form
+    {
+        private Label label1;
+        private TextBox txtOperand1;
+        private TextBox txtOperand2;
+        private TextBox txtResult;
+        private Button btnCalc;
+        private Button button2;
+        private Label label2;
+        #region Windows Code
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter first integer value";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(43, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter second integer value";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(193, 31);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand1.TabIndex = 2;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(193, 65);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand2.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(43, 112);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(305, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(64, 152);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "E&xit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(386, 187);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Integer Division";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static void Main()
+        {
+            frmMain main = new frmMain();
+            Application.Run(main);
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int operand1;
+            int operand2;
+            int answer;
+            // Input Step
+            // Check first input...
+            flag = int.TryParse(txtOperand1.Text, out operand1);
+            if (flag == false)
+            {
+                MessageBox.Show("Enter a whole number", "Input Error");
+                txtOperand1.Focus();
+                return;
+            }
+            // Check second input...
+            flag = int.TryParse(txtOperand2.Text, out operand2);
+            if (flag == false)
+            {
+                MessageBox.Show("Enter a whole number", "Input Error");
+                txtOperand2.Focus();
+                return;
+            }
+            // Process Step
+            answer = operand1 / operand2;
+            // Display Step
+            txtResult.Text = operand1.ToString() + " divided by " +
+            operand2.ToString() +
+            " equals " + answer.ToString();
+            txtResult.Visible = true;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
